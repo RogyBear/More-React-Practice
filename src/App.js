3da302bb111caac15a10036f0b80680141dec776
Aaron <rogersac24@gmail.com>
2019-09-10 14:14:16 +0500

    adding completed render component onclick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './App.css';
+import Question from './Question';
 
-function App() {
-  return (
-    <div className="App">
-      This is up
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			questions: [
+				{ id: 'fdsd', title: 'Why is the sky blue?' },
+				{ id: 'adsf', title: 'Who invented pizza?' },
+				{ id: 'afdsf', title: 'Is green tea overrated?' }
+			],
+			displayQuestions: false
+		};
+		this.displayQuestions = this.displayQuestions.bind(this);
+	}
+
+	displayQuestions = () => {
+		this.setState({
+			displayQuestions: !this.state.displayQuestions
+		});
+	};
+
+	render() {
+		let questions = null;
+		if (this.state.displayQuestions) {
+			questions = (
+				<div>
+					{this.state.questions.map((question, index) => {
+						return <Question key={question.id} title={question.title} />;
+					})}
+				</div>
+			);
+		}
+		return (
+			<div className="App">
+				<button className="btn" onClick={this.displayQuestions}>
+					View Unanswered Questions
+				</button>
+				{questions}
+			</div>
+		);
+	}
 }
 
 export default App;
